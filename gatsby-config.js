0f1730579488de63019f95e7f8c27b5fe6aa0b36
Toni Castillo <tonicastillo@gmail.com>
2020-05-13 13:34:28 +0200

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+const postcssPresetEnv = require('postcss-preset-env')
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Restaurante El Astronauta Córdoba`,
+    description: ``,
+    author: `Estudio Pablo Gallego`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,20 +15,56 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          postcssPresetEnv({
+            browsers: '> 0.5%, last 2 versions, ie 11'
+          })
+        ],
+      },
+    },
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#cccccc`,
+        theme_color: `#cccccc`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/stylesimages/favicon.png`, // This path is relative to the root of the site.
       },
     },
+
+    // {
+    //   resolve: `gatsby-plugin-gdpr-cookies`,
+    //   options: {
+    //     googleAnalytics: {
+    //       trackingId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+    //       // Setting this parameter is optional
+    //       anonymize: true
+    //     },
+    //     facebookPixel: {
+    //       pixelId: 'YOUR_FACEBOOK_PIXEL_ID'
+    //     },
+    //     // Defines the environments where the tracking should be available  - default is ["production"]
+    //     environments: ['production', 'development']
+    //   },
+    // },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
