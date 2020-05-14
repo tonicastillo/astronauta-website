70f572c10e691f92e2870558e84a45106409cf4f
Toni Castillo <tonicastillo@gmail.com>
2020-05-14 10:32:04 +0200

    downloads

---
+++ src/components/downloadicon.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Downloadicon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="15"
+      viewBox="0 0 16 15"
+    >
+      <path
+        fill="none"
+        stroke="#000"
+        strokeWidth="1"
+        d="M0 0L0 11"
+        data-name="Línea 11"
+        transform="translate(8)"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeWidth="1"
+        d="M3 6.361l5 5 5-5"
+        data-name="Trazado 37"
+      ></path>
+      {/* <path
+        fill="none"
+        stroke="#000"
+        strokeWidth="1"
+        d="M0 0L16 0"
+        data-name="Línea 12"
+        transform="translate(0 14.5)"
+      ></path> */}
+    </svg>
+  );
+}
+
+export default Downloadicon;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,10 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import styles from "./index.module.scss"
 
+import Downloadicon from "../components/downloadicon"
+
+import pdf1 from '../downloads/carta1.pdf' 
+import pdf2 from '../downloads/carta2.pdf' 
 
 const IndexPage = () => (
   <Layout>
@@ -14,9 +18,17 @@ const IndexPage = () => (
       <div className={styles.container_internal}>
         <header>
           <h1><span>El Astronauta</span></h1>
+          <div className={styles.descargas_escritorio}>
+            <a href={pdf1} download><Downloadicon />Carta restaurante (PDF)</a>
+            <a href={pdf2} download><Downloadicon />Carta para recoger (PDF)</a>
+          </div>
         </header>
         <main>
           <Image />
+          <div className={styles.descargas_movil}>
+            <a href={pdf1} download><Downloadicon />Carta restaurante (PDF)</a>
+            <a href={pdf2} download>Carta para recoger (PDF)</a>
+          </div>
         </main>
       </div>
     </div>
